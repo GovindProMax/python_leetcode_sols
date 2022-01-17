b88efc6d68c98b34e770fb47182c92e4a6033533
GovindProMax <hargovind.bling@gmail.com>
2022-01-16 18:57:52 -0700

    2 ques

--- house-robber.py
+++ house-robber.py
@@ -8,3 +8,22 @@ class Solution:
             firstrob=secondrob
             secondrob=temp
         return secondrob
+##### Alternate ######
+## found at https://leetcode.com/problems/house-robber/discuss/846235/Python-Simple-Solution-Explained-(video-%2B-code
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        if len(nums) <= 2:
+            return max(nums)
+        
+        dp = [0] * len(nums)
+        
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for x in range(2, len(nums)):
+            dp[x] = max(dp[x - 1], nums[x] + dp[x - 2])
+        
+        return dp[-1]
