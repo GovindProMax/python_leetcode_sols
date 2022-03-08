fcb2c7fed23ffa7c83bc42cb1f07fb69151e40b9
GovindProMax <hargovind.bling@gmail.com>
2022-03-07 20:17:19 -0700

    meh

--- missing-number.py
+++ missing-number.py
@@ -11,3 +11,12 @@ class Solution:
                 return counter
         return counter
         
+## Better Solution ##
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n=len(nums)
+        idealArr=n*(n+1)/2
+        mysum=0
+        for num in nums:
+            mysum=mysum+num
+        return int(idealArr - mysum)
