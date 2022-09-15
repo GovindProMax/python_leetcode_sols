642b883c5717ef83578ed49b0fb25f42dcfadd1b
GovindProMax <hargovind.bling@gmail.com>
2022-09-15 15:30:51 -0600

    new way

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -1,5 +1,23 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:  
-        new=re.sub('[^A-Za-z0-9]+','', s).lower()
-        return new == new[::-1]
+        
+        left=0
+        right = len(s)-1
+        
+        while left < right:
+            while (left < right) and self.isAlphaNum(s[left]) == False:
+                left+=1
+            while (left < right) and self.isAlphaNum(s[right]) == False:
+                right-=1
+                
+            if s[left].lower() != s[right].lower():
+                return False
+            left+=1
+            right-=1
+        return True
+    def isAlphaNum(self, s) -> bool:
+        return((ord('A')<=ord(s)<=ord('Z'))or(ord('a')<=ord(s)<=ord('z'))or(ord('0')<=ord(s)<=ord('9')))
+        
+        
+                
