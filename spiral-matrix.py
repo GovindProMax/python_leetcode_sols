4cc4b0a4f2e77ce2a88c22b588d19c95a9630f56
GovindProMax <hargovind.bling@gmail.com>
2023-03-15 18:24:06 -0600

    intuitive solution

--- spiral-matrix.py
+++ spiral-matrix.py
@@ -1,25 +1,23 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        startR,rows = 0,len(matrix)
-        startC,cols = 0,len(matrix[0])
+    def spiralOrder(self, array: List[List[int]]) -> List[int]:
+        startR,rows = 0,len(array)-1
+        startC,cols = 0,len(array[0])-1
         newMatrix=[]
-        while startR<rows and startC < cols:
-            
-            for i in range(startC,cols):
-                newMatrix.append(matrix[startR][i])
+        while startR<=rows and startC <=cols:
+            for c in range(startC,cols+1):
+                newMatrix.append(array[startR][c])
+            for r in range(startR+1,rows+1):
+                newMatrix.append(array[r][cols])
+            for c in range(cols-1,startC-1,-1):
+                if startR==rows:
+                    break
+                newMatrix.append(array[rows][c])
+            for r in range(rows-1,startR,-1):
+                if startC==cols:
+                    break
+                newMatrix.append(array[r][startC])
             startR+=1
-            
-            for i in range(startR,rows):
-                newMatrix.append(matrix[i][cols - 1])
-            cols-=1
-            if not(startR<rows and startC < cols):
-                break
-            for i in range(cols,startC,-1):
-                newMatrix.append(matrix[rows - 1][i -1])
-            rows-=1
-                
-            for i in range(rows,startR,-1):
-                newMatrix.append(matrix[i-1][startC])
             startC+=1
+            rows-=1
+            cols-=1
         return newMatrix
-            
