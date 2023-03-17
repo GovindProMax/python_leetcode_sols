072cd319494e64e86a386f0853a58387ce6f7151
GovindProMax <hargovind.bling@gmail.com>
2023-03-17 14:00:53 -0600

    3rd solution added

--- fint-the-duplicate-number.py
+++ fint-the-duplicate-number.py
@@ -1,6 +1,8 @@
 class Solution:
     def findDuplicate(self, array: List[int]) -> int:
-        """dict = {}
+        """
+        O(n) space solution
+        dict = {}
         for index in range(len(array)):
             if array[index] in dict:
                 return array[index]
@@ -9,7 +11,8 @@ class Solution:
         return -1*/
         
         Floyd's algorithm:
-        """
+        O(1) space solution without modifying array
+        
         
         slow,fast=0,0
 
@@ -25,3 +28,14 @@ class Solution:
             slow2=array[slow2]
             if slow==slow2:
                 return slow
+                
+        When you can modify the array:
+        """
+    def findDuplicate(self, array: List[int]) -> int:
+        for value in array:
+            absVal = abs(value)
+
+            if array[absVal-1]<0:
+                return absVal
+            array[absVal-1]*=-1
+        return -1
